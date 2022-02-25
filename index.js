4c2b9036c54dfeab224916e2a1de97b7c6c251bf
sivap2000 <100260449+sivap2000@users.noreply.github.com>
2022-02-24 22:53:38 -0800

    Parent Component for react-made mini-Amazon file

---
+++ index.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import ReactDOM from "react-dom";
+//import Api from './Api';
+ 
+//  import {useState} from 'react';
+import RouterPage from './Components/RouterPage';
+
+// import ProductsMain from './CardAdding/ProductsMain';
+// import {BrowserRouter,Routes,Route} from 'react-router-dom';
+// import About from "./BootstrapWebsite/About";
+// import Home from "./BootstrapWebsite/Home";
+// import Navigation from "./BootstrapWebsite/Navigation";
+// import ProductsMain from "./BootstrapWebsite/ProductsMain";
+// import Services from "./BootstrapWebsite/Services";
+// import Extra from './Extra';
+ 
+//  function MyWebsite(){
+//             return (
+//                <BrowserRouter>
+//                   <Navigation />
+//                   <Routes>
+//                      <Route path='/' element={<Home />}></Route>
+//                      <Route path='/ProductsMain' element={<ProductsMain />}></Route>
+//                      <Route path='/About' element={<About />}></Route>
+//                      <Route path='/Services' element={<Services />}></Route>
+//                   </Routes>
+//                </BrowserRouter>
+//             )
+//       } 
+
+
+               
+                 
+
+
+          
+    ReactDOM.render(<RouterPage />, document.getElementById("root"))
